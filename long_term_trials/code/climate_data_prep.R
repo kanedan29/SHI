8fb294887e48a18a7b34afca2529b3d34246f137
Connor Reed <connor@Connors-MacBook-Pro.local>
2019-09-30 19:08:54 +1300

    Geolocate studies and begin adding climate data

---
+++ long_term_trials/code/climate_data_prep.R
@@ -0,0 +1,208 @@
+source("code/libraries.R")
+
+load("data/d.carbon.RData")
+load("data/d.yield.RData")
+
+d.yield <- d
+
+d.yield %>%
+  group_by(Paper, Study_name, begin_obs, end_obs) %>%
+  count() %>%
+  select(Paper, Study_name, begin_obs, end_obs) -> yield.climate
+
+d.carbon %>%
+  group_by(Paper, Study_name, Year_of_observation) %>%
+  count() %>%
+  select(Paper, Study_name, Year_of_observation) -> carbon.climate
+
+# Need to clean up location names to searchable formats; default to city, country
+# with more specificity if available
+
+yield.climate <- yield.climate %>%
+  mutate(Location = Study_name) %>%
+  mutate(Location = str_replace_all(Location, 
+                                c("Makoka Agricultural Research Station" = "Thondwe, Malawi",
+                                  "Bulbertson" = "Culbertson",
+                                  "Pietranera farm, Universita degli Studi di Palermo" = "Pietranera farm, Agrigento, Sicily, Italy",
+                                  "Research Station of the Riograndense Rice Institute \\(IRGA\\), Cachoeirinha City, Rio Grande do Sul State, Southern Brazil" = "Cachoeirinha City, Rio Grande do Sul State, Brazil",
+                                  "Purdue University Agronomy Center for Research and Education \\(ACRE\\), West Lafayette, Indiana" = "West Lafayette, IN",
+                                  "La Canaleja \\(INIA\\), " = "",
+                                  "USDA-ARS Conservation and Production Research Laboratory, " = "",
+                                  "USDA-ARTS Central Great Plains Research Station, " = "",
+                                  "Loess Plateau, Chenghuang village, " = "",
+                                  "University Farm, Institue for Sustainable Agro-ecosystem Services, " = "",
+                                  "Southern Illinois University Belleville Research Center, " = "",
+                                  "Mississippi Agricultural and Forestry Experiment Station at Holly Springs" = "Holly Springs, MS",
+                                  "Macdonald Campus Research Farm, McGill University, " = "",
+                                  "Southeast Norway, morainic loam soil" = "Hedmark, Norway",
+                                  "USDA-ARS J. Phil Campbell Sr. Natural Resource Conservation Center, " = "",
+                                  "No name" = "Clay Center, NE", # Based on statement in paper that manure was transported from nearby US Meat Animal Research Center
+                                  "La Chimenea field Station, " = "",
+                                  "Columbia Basin Agricultural Research Center, " = "",
+                                  "Yadkin Valley Vineyard, North Carolina, USA" = "New Hope, NC",
+                                  "Hayathnagar Research Farm, Central Research Institute for Dryland Agriculture, " = "",
+                                  "Macdonald Research Farm of McGill University, Ste-Anne de Beelevue, " = "Ste-Anne-de-Bellevue, ",
+                                  "Hyndevad, Jutland, Denmark \\s+Odum, Jutland, Denmark" = "Jutland, Denmark",
+                                  "Shang Tuhe village, " = "",
+                                  "Instituto Madrileno de Investigacion y Desarollo Rural Agrario y Alimentario Experimental Station, " = "",
+                                  "Farm of Southwest Agricultural University, " = "",
+                                  "Beiqiu, " = "",
+                                  "Research farm, Rajendra Agricultural University, " = "",
+                                  "Haycreek Township, Goodhue Country, Minnesota, USA" = "Red Wing, MN",
+                                  "Clemson University Research and Education Center, " = "",
+                                  "Iowa State University \\(ISU\\) Agronomy/Ag Engineering Research and Education Center, Boone County, Iowa, USA" = "Boone County, IA",
+                                  "Research farm, International Institute of Tropical Agriculture \\(IITA\\), Ihadan, " = "Ibadan, ",
+                                  "Tidewater Research Station, " = "",
+                                  "International Center for Agricultural Research in the Dry Areas \\(ICARDA\\), Tel Hadya, " = "",
+                                  "Oklahoma Pandhandle Research and Extension Center \\(OPREC\\), " = "",
+                                  "Northwestern Agricultural Research Station, " = "",
+                                  "Northwestern Branch, " = "",
+                                  "Ohio Agricultural Research and Development Center, " = "",
+                                  "Waterman Farm, " = "",
+                                  "Site = Agramunt" = "Agramunt, Catalonia, Spain",
+                                  "Site = El Canos" = "El Canos, Catalonia, Spain",
+                                  "Site = Selvanera" = "Selvanera, Catalonia, Spain",
+                                  "University of Minnesota Agricultural Experiment Station, " = "",
+                                  "Palouse Conservation Field Station, " = "",
+                                  "Shiwang Village, " = "",
+                                  "Enrico Avanzi Interdepartmental Center for Agro-Environmental Research, " = "",
+                                  "Agriculture and Agri-Food Canada Research Centre, " = "",
+                                  "Cantuar, " = "Swift Current, ",
+                                  "Horticultural Experiment Station, " = "",
+                                  "L'Acadie Research Station, Agriculture and Agri-Food Canada" = "St-Jean-sur-Richelieu, Quebec",
+                                  "Swedish University of Agricultural Sciences, " = "",
+                                  "Eastern South Dakota Soil and Water Research Farm, " = "",
+                                  "Rasmussen dryland farm site, " = "",
+                                  "Regional Agricultural Research Station, Bhairhawa, " = "Bhairahawa, ",
+                                  "Baden-Wurttemberg site" = "Forchtenberg, Germany",
+                                  "Fire" = "Forchtenberg, Germany",
+                                  "Forchtenberg site" = "Forchtenberg, Germany",
+                                  "Stagnic Luvisol" = "Forchtenberg, Germany",
+                                  "Haplic Luvisol" = "Forchtenberg, Germany",
+                                  "Fort Ellis Research and Extension Center, Montana State University, " = "",
+                                  "Hisar" = "Hisar, India",
+                                  "Solapur" = "Solapur, India",
+                                  "Eastern site" = "Horse Heaven, WA",
+                                  "Western site" = "Horse Heaven, WA",
+                                  "University of Nebraska Agricultural Research and Development Center, " = "",
+                                  "Tennessee Valley Research and Extension Center, Alabama Agriculture Experiment Station, " = "",
+                                  "Taihu Lake region, China" = "Taihu Lake, China",
+                                  "Asasa research stations, south-eastern highlands, Ethiopia" = "Asassa, Ethiopia",
+                                  "Kulumsa research stations, south-eastern highlands, Ethiopia" = "Kulumsa, Ethiopia",
+                                  "Shenmu erosion and environmental testing station, Institute of Soil and Water Conservation, Shen Mu county, " = "",
+                                  "Chilato village, Zimuto Communal Area \\(Chikato\\), " = "",
+                                  "Henderson Research Station, " = "",
+                                  "Hereford Farm, " = "",
+                                  "National Agriculture Research Center, " = "",
+                                  "ADAS Bridgets, " = "",
+                                  "ADAS Drayton, " = "",
+                                  "ADAS High Mowthorpe, " = "",
+                                  "ADAS Terrington, " = "",
+                                  "Campo Experimental Norman E. Borlaug, " = "",
+                                  "University of California Coachella Valley Agricultural Research Station, " = "",
+                                  "Sand Mountain Research and Extension Center, Appalachian Plateau region, " = "",
+                                  "Southern Loess Plateau" = "Zhouzhi, Xi'an, Shaanxi, China",
+                                  "L'Acadie Experimental Farm of Agriculture and Agri-Food Canada, " = "St-Jean-sur-Richelieu, ")))
+
+carbon.climate <- carbon.climate %>%
+  mutate(Location = Study_name) %>%
+  mutate(Location = str_replace_all(Location,
+                                   c("Oklahoma Pandhandle Research and Extension Center \\(OPREC\\), " = "",
+                                     "Southeast Norway, morainic loam soil" = "Hedmark, Norway",
+                                     "La Chimenea field Station, " = "",
+                                     "Columbia Basin Agricultural Research Center, " = "",
+                                     "Shang Tuhe village, " = "",
+                                     "Farm of Southwest Agricultural University, " = "",
+                                     "Beiqiu, " = "",
+                                     "Research farm, Rajendra Agricultural University, " = "",
+                                     "Clemson University Research and Education Center, " = "",
+                                     "Northwestern Agricultural Research Station, " = "",
+                                     "Ohio Agricultural Research and Development Center, " = "",
+                                     "Waterman Farm, " = "",
+                                     "Pietranera farm, Universita degli Studi di Palermo" = "Pietranera farm, Agrigento, Sicily, Italy",
+                                     "Enrico Avanzi Interdepartmental Center for Agro-Environmental Research, " = "",
+                                     "L'Acadie Research Station, Agriculture and Agri-Food Canada" = "St-Jean-sur-Richelieu, Quebec",
+                                     "Eastern South Dakota Soil and Water Research Farm, " = "",
+                                     "Regional Agricultural Research Station, Bhairhawa, " = "Bhairahawa, ",
+                                     "University of Nebraska Agricultural Research and Development Center, " = "",
+                                     "Shenmu erosion and environmental testing station, Institute of Soil and Water Conservation, Shen Mu county, " = "",
+                                     "Chilato village, Zimuto Communal Area \\(Chikato\\), " = "",
+                                     "Henderson Research Station, " = "",
+                                     "Hereford Farm, " = "",
+                                     "ADAS Bridgets, " = "",
+                                     "ADAS Drayton, " = "",
+                                     "ADAS High Mowthorpe, " = "",
+                                     "ADAS Terrington, " = "",
+                                     "Southern Loess Plateau" = "Zhouzhi, Xi'an, Shaanxi, China"
+                                     )))
+  
+  
+yield.climate %>%
+  group_by(Paper, Location) %>%
+  select(Paper, Location) %>%
+  distinct(Paper, Location) -> yield.locations
+  
+carbon.climate %>%
+  group_by(Paper, Location) %>%
+  select(Paper, Location) %>%
+  distinct(Paper, Location) -> carbon.locations
+
+
+yield.locations$lat <- NA
+yield.locations$lon <- NA
+
+
+get_geo = function(study_name){
+  result <- geocode_OSM(study_name, return.first.only = TRUE, as.data.frame = TRUE)
+  return(result)
+}
+
+# Takes awhile
+
+for (i in 1:nrow(yield.locations)){
+  geo <- try(get_geo(locations$Location[i]))
+  if(inherits(geo, "try-error"))
+  {
+    next
+  }
+  else
+  {
+    yield.locations$lat[i] <- geo["lat"]
+    yield.locations$lon[i] <- geo["lon"]
+  }
+}
+
+yield.locations$lat[which(locations$Location=="Cangwu Agri-ecological Station of the Loess Plateau, China")] <- 35.200
+yield.locations$lon[which(locations$Location=="Cangwu Agri-ecological Station of the Loess Plateau, China")] <- 107.667
+
+yield.climate <- yield.climate %>%
+  left_join(yield.locations, by = c("Paper", "Location"))
+
+carbon.climate <- carbon.climate %>%
+  left_join(yield.locations, by = c("Paper", "Location"))
+
+################################################################################
+# Climate data
+
+# World Bank Climate Data API
+# River basin ids and polygon mappings from https://github.com/hrbrmstr/basins.git
+
+
+layers <- GET("https://landgisapi.opengeohub.org/query/layers")
+
+
+
+path <- "http://landgisapi.opengeohub.org/query/point?"
+test.response <- 
+
+
+
+  
+
+
+
+
+
+
+
+
